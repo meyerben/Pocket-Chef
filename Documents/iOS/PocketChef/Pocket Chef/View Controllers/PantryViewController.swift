68ad9338a31d3d1059da0031a127155bce928cdb
JayPhippss <PhippsJaylin@gmail.com>
2018-11-15 12:25:48 -0600

    Add to pantry complete

--- Documents/iOS/PocketChef/Pocket Chef/View Controllers/PantryViewController.swift
+++ Documents/iOS/PocketChef/Pocket Chef/View Controllers/PantryViewController.swift
@@ -26,12 +26,16 @@ class PantryViewController: UIViewController, UITableViewDelegate, UITableViewDa
         //NEED TO STORE ARRAY IN CORE DATA
         let pantryAlert = UIAlertController(title: "Add Item", message: "Enter the item you would like to add to your pantry.", preferredStyle: .alert)
         
+        //Check to make sure its not empty
         pantryAlert.addTextField { (textField) in
-            textField.text = "Type Item Here"
+            textField.text = ""
         }
         
         pantryAlert.addAction(UIAlertAction(title: "Add", style: .default, handler: { (_) in
-            var textField = pantryAlert.textFields![0]
+            let textField = pantryAlert.textFields![0]
+            
+            self.pantryArray.append(textField.text!)
+            self.pantryTblView.reloadData()
         }))
         
         self.present(pantryAlert, animated: true, completion: nil)
